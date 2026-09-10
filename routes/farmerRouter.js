f75dd17548fc36d237df122865e1ad066ffb4a5d
Parth More <parthmore827@gmail.com>
2026-09-11 00:42:13 +0530

    Use database connection pools

--- routes/farmerRouter.js
+++ routes/farmerRouter.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // DATABASE CONNECTION
 // =====================================
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT || 4000),
     user: process.env.DB_USER,
